ee19b1f05c2b4631f1d2eb4df2cb1c5eb7f0303e
gr33nimax <noname.play@yandex.ru>
2026-08-14 21:41:25 +0300

    publish GitHub-built debug APK

--- test/release_workflow_test.dart
+++ test/release_workflow_test.dart
@@ -48,6 +48,7 @@ void main() {
     expect(workflow, contains(':app:lintDebug'));
     expect(workflow, contains(':app:assembleDebug'));
     expect(workflow, contains(':app:assembleDebugAndroidTest'));
+    expect(workflow, contains('build/app/outputs/apk/debug/*.apk'));
     expect(workflow, contains('Build debug-signed release-mode test APK'));
     expect(workflow, contains('HYDRABOX_ALLOW_DEBUG_RELEASE_SIGNING'));
     expect(workflow, contains('Upload test APK'));
