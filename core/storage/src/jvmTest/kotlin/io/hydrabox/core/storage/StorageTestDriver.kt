a2b565b1a3c57bf73e1fa295bae1c47f42d6d578
gr33nimax <noname.play@yandex.ru>
2026-09-01 09:29:42 +0300

    fix(h2): migrate storage test from schema v2
    Task: H2-D01

--- core/storage/src/jvmTest/kotlin/io/hydrabox/core/storage/StorageTestDriver.kt
+++ core/storage/src/jvmTest/kotlin/io/hydrabox/core/storage/StorageTestDriver.kt
@@ -6,6 +6,8 @@ import app.cash.sqldelight.driver.jdbc.sqlite.JdbcSqliteDriver
 object StorageTestDriver {
     fun previousVersion(): SqlDriver = JdbcSqliteDriver(JdbcSqliteDriver.IN_MEMORY).also { driver ->
         driver.execute(null, "CREATE TABLE settings (setting_key TEXT NOT NULL PRIMARY KEY, value TEXT NOT NULL)", 0)
+        driver.execute(null, "ALTER TABLE settings ADD COLUMN secret_value BLOB", 0)
+        driver.execute(null, "CREATE TABLE storage_metadata (storage_key TEXT NOT NULL PRIMARY KEY, value BLOB NOT NULL)", 0)
         driver.execute(null, "INSERT INTO settings(setting_key, value) VALUES ('theme', 'dark')", 0)
     }
 
